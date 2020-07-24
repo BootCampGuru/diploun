95b7f76546a8856df4524d0cfe98f183c3aca0c5
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-07-24 17:56:09 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -494,7 +494,7 @@ return <GeoJSON  key='my-geojson' data={this.state.world_map} />
         //console.log(each.Longitude)
         if (isNaN(each.Longitude) === false && isNaN(each.Latitude) === false) {
         var position=[each.Latitude, each.Longitude]
-        return <Marker key={index} position={position} icon={myIcon}>
+        return <Marker onClick={this.onWeatherChanged} key={index} position={position} icon={myIcon}>
           <Popup><br /> 
             <img alt="pic" style={{width:"150px"}} src={each.Staff_Image} /><br/>
             <a target='_blank' href='{each.Staff_Url}' ><b>{each.Staff_Name}</b></a><br/>
