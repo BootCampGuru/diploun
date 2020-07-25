4a667b140a845e6b61a47f9209dae042437cb445
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-07-24 23:11:17 -0400

    changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/diploun/precache-manifest.4a938a816f05b234921a26133901c2e0.js"
+  "/diploun/precache-manifest.b78db0fb43522f8941b6f24e3557f28c.js"
 );
 
 self.addEventListener('message', (event) => {
