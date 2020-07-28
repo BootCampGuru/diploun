87b635cbe91ff2fcf77bfc624fd724c53ef111c2
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-07-28 12:34:00 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -544,7 +544,7 @@ return <GeoJSON  key='my-geojson' data={this.state.world_map} />
         //console.log(each.Longitude)
         if (isNaN(each.Longitude) === false && isNaN(each.Latitude) === false) {
         var position=[each.Latitude, each.Longitude]
-        return <Marker key={index} position={position} icon={each.Property_Name === 'CONSULATE' ? myIcon : mySecondIcon}>
+        return <Marker key={index} position={position} icon={each.Property_Name === 'CONSULATE' ? mySecondIcon : myIcon}>
               <Popup><br /> 
                   <img alt="pic" style={{width:"150px"}} src={each.Staff_Image} /><br/>
                   <a target='_blank' href='{each.Staff_Url}' ><b>{each.Staff_Name}</b></a><br/>
