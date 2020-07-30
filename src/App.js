0f58c0a61502cce53b9128928da56d1283bebcfb
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-07-30 03:13:11 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -591,7 +591,7 @@ return <GeoJSON  key='my-geojson' data={this.state.world_map} />
         {
 
           this.state.show_advisory ? 
-          console.log(map_wildlifedata);
+         
           map_wildlifedata.map((each,index) => {
             return <GeoJSON  key={index} data={each} style={this.getColor(each.score)} />
           })
