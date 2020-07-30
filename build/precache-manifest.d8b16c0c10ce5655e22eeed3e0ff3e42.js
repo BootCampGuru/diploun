da954b5f607c37784dc4ccf266117ff3edf440c7
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-07-29 23:23:11 -0400

    changes

---
+++ build/precache-manifest.d8b16c0c10ce5655e22eeed3e0ff3e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6eeca0779167255079a507722bca29b",
+    "url": "/diploun/index.html"
+  },
+  {
+    "revision": "d447753713805c2a4d51",
+    "url": "/diploun/static/css/2.8e6c8c35.chunk.css"
+  },
+  {
+    "revision": "1a10909ed4d9a128626c",
+    "url": "/diploun/static/css/main.58e8cb2a.chunk.css"
+  },
+  {
+    "revision": "d447753713805c2a4d51",
+    "url": "/diploun/static/js/2.834f32c9.chunk.js"
+  },
+  {
+    "revision": "0569213fd349f089ee316434aa7e3e81",
+    "url": "/diploun/static/js/2.834f32c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a10909ed4d9a128626c",
+    "url": "/diploun/static/js/main.55be409c.chunk.js"
+  },
+  {
+    "revision": "0ea3a5d5ad355e36cb84",
+    "url": "/diploun/static/js/runtime-main.5f3dfea0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/diploun/precache-manifest.a49001fabcb9b2a8c02c258e32226282.js"
+  "/diploun/precache-manifest.d8b16c0c10ce5655e22eeed3e0ff3e42.js"
 );
 
 self.addEventListener('message', (event) => {
