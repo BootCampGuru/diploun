21beaee229ea702062899f9a808555ab1146c60a
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-08-03 02:55:12 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ var myGoogleIcon = L.icon({
   });
 
 var myIcon = L.icon({
-iconUrl: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
+iconUrl: './images/blue.png',
 iconSize: [25,41],
 iconAnchor: [12.5, 41],
 popupAnchor: [0, -41]
