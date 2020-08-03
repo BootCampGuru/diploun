23b434e85d0b85b6bfd832059d12ec1561a99dca
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-08-02 21:44:27 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ var myGoogleIcon = L.icon({
   });
 
 var myIcon = L.icon({
-iconUrl: './images/dot.gif',
+iconUrl: './images/dot.png',
 iconSize: [25,41],
 iconAnchor: [12.5, 41],
 popupAnchor: [0, -41]
