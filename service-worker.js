b89bc9aa4e5e46519f8c4055ac162eca57a92da7
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-08-04 09:10:18 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/diploun/precache-manifest.437bae1a91a72d80affab2b3e9113896.js"
+  "/diploun/precache-manifest.60781888d153f820b6bdc8fde8563fc8.js"
 );
 
 self.addEventListener('message', (event) => {
