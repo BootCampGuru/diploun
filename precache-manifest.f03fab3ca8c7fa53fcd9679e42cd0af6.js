563c69a2774c2e8be509d415ebd82d5813f583ab
Ranjan Biswas <ranjanbiswasgwu@gmail.com>
2020-08-04 01:24:37 -0400

    Updates

---
+++ precache-manifest.f03fab3ca8c7fa53fcd9679e42cd0af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252f05da65ca53473d46db072b4da027",
+    "url": "/diploun/index.html"
+  },
+  {
+    "revision": "7d07f4482b79a1878168",
+    "url": "/diploun/static/css/2.8e6c8c35.chunk.css"
+  },
+  {
+    "revision": "d2d300c1645c675c7b32",
+    "url": "/diploun/static/css/main.ef7dc971.chunk.css"
+  },
+  {
+    "revision": "7d07f4482b79a1878168",
+    "url": "/diploun/static/js/2.acb68699.chunk.js"
+  },
+  {
+    "revision": "0569213fd349f089ee316434aa7e3e81",
+    "url": "/diploun/static/js/2.acb68699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d300c1645c675c7b32",
+    "url": "/diploun/static/js/main.a9c94c90.chunk.js"
+  },
+  {
+    "revision": "0ea3a5d5ad355e36cb84",
+    "url": "/diploun/static/js/runtime-main.5f3dfea0.js"
+  }
+]);
